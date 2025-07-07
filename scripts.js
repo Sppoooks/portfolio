560ad67789ed20e6f0b2cef982e6d350b63fbf12
Thomas Wan <thomas.wann14@gmail.com>
2025-07-07 10:25:33 +0100

    Added more to navgiation bar

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
-console.log("Poop")
+let index = 1;
+
+function print_poop() {
+    console.log(index + " Poop");
+    index += 1;
+}
+
+document.getElementById("project_button").addEventListener("click", print_poop);
