322e60ed761da1cf356f55fc99b4c1abd984ab33
Thomas Wan <thomas.wann14@gmail.com>
2025-07-12 21:41:04 +0200

    Added dropdown animation

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
 function toggle_dropdown() {
     const dropdown = document.getElementById("project_dropdown_content");
+    const button = document.getElementById("project_button");
+
     dropdown.classList.toggle("show");
+    button.classList.toggle("project_button_click");
 }
