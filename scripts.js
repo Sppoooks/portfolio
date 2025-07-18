dc45a7c9a0b0346e0c2423ae7b7234244c93f3f4
Thomas Wan <thomas.wann14@gmail.com>
2025-07-18 19:30:26 +0200

    Made front page mobile friendly

--- scripts.js
+++ scripts.js
@@ -1,7 +1,19 @@
-function toggle_dropdown() {
-    const dropdown = document.getElementById("project_dropdown_content");
-    const button = document.getElementById("project_button");
+const dropdown = document.getElementById("project_dropdown_content");
 
+const button = document.getElementById("project_button");
+
+const burger_menu = document.getElementById("burger-menu");
+
+const nav = document.getElementById("homepage_navigation");
+
+function toggle_dropdown() {
     dropdown.classList.toggle("show");
     button.classList.toggle("project_button_click");
 }
+
+function fade_animation() {
+    burger_menu.classList.toggle("active")
+    nav.classList.toggle("active")
+}
+
+burger_menu.addEventListener("click", fade_animation)
